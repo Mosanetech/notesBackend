416c3c048a2356b4f2bf6f2abdfb3b509ceaff73
Mosam04 <bsc-com-ne-33-23@unima.ac.mw>
2025-08-28 22:39:14 +0200

    removing email and phone number validations

--- users/serializers/login_serializer.py
+++ users/serializers/login_serializer.py
@@ -12,21 +12,16 @@ class LoginSerializer(serializers.Serializer):
         if not identifier or not password:
             raise serializers.ValidationError("Both identifier and password are required.")
 
-        user = None
         
-        if "@" in identifier:
-            try:
-                user = CustomUser.objects.get(email=identifier)
-            except CustomUser.DoesNotExist:
-                raise serializers.ValidationError("Invalid login credentials.")
-        
-        elif identifier.isdigit():
+        try:
+            user = CustomUser.objects.get(email=identifier)
+        except CustomUser.DoesNotExist:
             try:
                 user = CustomUser.objects.get(phone_number=identifier)
             except CustomUser.DoesNotExist:
                 raise serializers.ValidationError("Invalid login credentials.")
 
-        if user is None or not user.check_password(password):
+        if not user.check_password(password):
             raise serializers.ValidationError("Invalid login credentials.")
 
         data['user'] = user
